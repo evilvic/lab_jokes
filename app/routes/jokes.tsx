ff053a29e3834536bf29b6da606bc016ffda9594
Víctor Peña Romero <vic.pero@icloud.com>
2023-02-07 08:10:25 -0600

    list jokes

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,6 +1,8 @@
-import { Outlet, Link } from "@remix-run/react";
+import { Outlet, Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
 
 import stylesUrl from "~/styles/jokes.css";
+import { db } from "~/utils/db.server";
 
 import type { LinksFunction } from "@remix-run/node";
 
@@ -8,7 +10,18 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
+export const loader = async () => {
+  return json({
+    jokes: await db.joke.findMany({
+      take: 5,
+      select: { id: true, name: true },
+      orderBy: { createdAt: "desc" },
+    }),
+  });
+};
+
 export default function JokesIndexRoute() {
+  const data = useLoaderData<typeof loader>()
   return (
     <div className="jokes-layout">
       <header className="jokes-header">
@@ -31,9 +44,11 @@ export default function JokesIndexRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              <li>
-                <Link to="some-joke-id">Hippo</Link>
-              </li>
+              {data.jokes.map((joke) => (
+                <li key={joke.id}>
+                    <Link to="some-joke-id">{joke.name}</Link>
+                </li>
+              ))}
             </ul>
             <Link to="new" className="button">
               Add your own
