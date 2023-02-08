00de02e063b473083c02ffe3d501e0bd2b43ece5
Víctor Peña Romero <vic.pero@icloud.com>
2023-02-08 08:07:25 -0600

    add random joke

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,11 +1,27 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [joke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+  return json({ joke })
+}
+
 export default function JokesIndexRoute() {
+  const data = useLoaderData<typeof loader>()
   return (
     <div>
       <p>Here's a random joke:</p>
       <p>
-        I was wondering why the frisbee was getting bigger,
-        then it hit me.
+        {data.joke.content}
       </p>
+      <Link to=".">{data.joke.name} Permalink</Link>
     </div>
   );
 }
