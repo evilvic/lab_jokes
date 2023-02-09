2abe47e2e32cb57eec67e6857c12c17331293296
Víctor Peña Romero <vic.pero@icloud.com>
2023-02-09 08:41:39 -0600

    start validating login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,12 +1,109 @@
-import { Link, useSearchParams } from "@remix-run/react";
+import { Link, useActionData, useSearchParams } from "@remix-run/react";
+import { json } from "@remix-run/node";
 
 import stylesUrl from "~/styles/login.css";
+import { db } from "~/utils/db.server";
+import { badRequest } from "~/utils/request.server";
 
-import type { LinksFunction } from "@remix-run/node";
+import type { ActionArgs, LinksFunction } from "@remix-run/node";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesUrl },
 ];
+
+function validateUsername(username: unknown) {
+  if (typeof username !== "string" || username.length < 3) {
+    return `Usernames must be at least 3 characters long`;
+  }
+}
+
+function validatePassword(password: unknown) {
+  if (typeof password !== "string" || password.length < 6) {
+    return `Passwords must be at least 6 characters long`;
+  }
+}
+
+function validateUrl(url: string) {
+  let urls = ["/jokes", "/", "https://remix.run"];
+  if (urls.includes(url)) {
+    return url;
+  }
+  return "/jokes";
+}
+
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData();
+  const loginType = form.get("loginType");
+  const username = form.get("username");
+  const password = form.get("password");
+  const redirectTo = validateUrl(
+    form.get("redirectTo") || "/jokes"
+  );
+  if (
+    typeof loginType !== "string" ||
+    typeof username !== "string" ||
+    typeof password !== "string" ||
+    typeof redirectTo !== "string"
+  ) {
+    return badRequest({
+      fieldErrors: null,
+      fields: null,
+      formError: `Form not submitted correctly.`,
+    });
+  }
+
+  const fields = { loginType, username, password };
+  const fieldErrors = {
+    username: validateUsername(username),
+    password: validatePassword(password),
+  };
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({
+      fieldErrors,
+      fields,
+      formError: null,
+    });
+  }
+
+  switch (loginType) {
+    case "login": {
+      // login to get the user
+      // if there's no user, return the fields and a formError
+      // if there is a user, create their session and redirect to /jokes
+      return badRequest({
+        fieldErrors: null,
+        fields,
+        formError: "Not implemented",
+      });
+    }
+    case "register": {
+      const userExists = await db.user.findFirst({
+        where: { username },
+      });
+      if (userExists) {
+        return badRequest({
+          fieldErrors: null,
+          fields,
+          formError: `User with username ${username} already exists`,
+        });
+      }
+      // create the user
+      // create their session and redirect to /jokes
+      return badRequest({
+        fieldErrors: null,
+        fields,
+        formError: "Not implemented",
+      });
+    }
+    default: {
+      return badRequest({
+        fieldErrors: null,
+        fields,
+        formError: `Login type invalid`,
+      });
+    }
+  }
+};
 
 export default function Login() {
   const [searchParams] = useSearchParams();
