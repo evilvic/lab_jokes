bb8a2bd39c543ab0c4f8bdedbb49ed32c2c2c96e
Víctor Peña Romero <vic.pero@icloud.com>
2023-02-10 08:08:41 -0600

    finish login validations

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -106,6 +106,7 @@ export const action = async ({ request }: ActionArgs) => {
 };
 
 export default function Login() {
+  const actionData = useActionData<typeof action>();
   const [searchParams] = useSearchParams();
   return (
     <div className="container">
@@ -128,7 +129,10 @@ export default function Login() {
                 type="radio"
                 name="loginType"
                 value="login"
-                defaultChecked
+                defaultChecked={
+                  !actionData?.fields?.loginType ||
+                  actionData?.fields?.loginType === "login"
+                }
               />{" "}
               Login
             </label>
@@ -137,6 +141,10 @@ export default function Login() {
                 type="radio"
                 name="loginType"
                 value="register"
+                defaultChecked={
+                  actionData?.fields?.loginType ===
+                  "register"
+                }
               />{" "}
               Register
             </label>
@@ -147,7 +155,25 @@ export default function Login() {
               type="text"
               id="username-input"
               name="username"
+              defaultValue={actionData?.fields?.username}
+              aria-invalid={Boolean(
+                actionData?.fieldErrors?.username
+              )}
+              aria-errormessage={
+                actionData?.fieldErrors?.username
+                  ? "username-error"
+                  : undefined
+              }
             />
+            {actionData?.fieldErrors?.username ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="username-error"
+              >
+                {actionData.fieldErrors.username}
+              </p>
+            ) : null}
           </div>
           <div>
             <label htmlFor="password-input">Password</label>
@@ -155,7 +181,35 @@ export default function Login() {
               id="password-input"
               name="password"
               type="password"
+              defaultValue={actionData?.fields?.password}
+              aria-invalid={Boolean(
+                actionData?.fieldErrors?.password
+              )}
+              aria-errormessage={
+                actionData?.fieldErrors?.password
+                  ? "password-error"
+                  : undefined
+              }
             />
+            {actionData?.fieldErrors?.password ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="password-error"
+              >
+                {actionData.fieldErrors.password}
+              </p>
+            ) : null}
+          </div>
+          <div id="form-error-message">
+            {actionData?.formError ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+              >
+                {actionData.formError}
+              </p>
+            ) : null}
           </div>
           <button type="submit" className="button">
             Submit
